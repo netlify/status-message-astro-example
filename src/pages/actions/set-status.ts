f933d90c6c005a9dbecff3ea22932c751f2dd628
Sean C Davis <scdavis41@gmail.com>
2024-03-20 06:50:15 -0400

    Show custom success message

--- src/pages/actions/set-status.ts
+++ src/pages/actions/set-status.ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from "astro";
 
-export const POST: APIRoute = async ({ cookies, redirect }) => {
-  cookies.set("status", "Success!!!", {
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+  const formData = await request.formData();
+  const message = formData.get("message");
+
+  cookies.set("status", message || "(message not set)", {
     httpOnly: true,
     sameSite: "strict",
     path: "/",
